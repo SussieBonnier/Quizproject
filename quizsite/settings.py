2a23df6098c747a1d6a7c0addce9c12127863c59
SussieBonnier <32262172+SussieBonnier@users.noreply.github.com>
2017-09-27 15:37:47 +0200

    Added bootstrap.css

--- quizsite/settings.py
+++ quizsite/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'quiz',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -118,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
