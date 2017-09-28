514260632bff86501f74868be63c815ee2178638
SussieBonnier <32262172+SussieBonnier@users.noreply.github.com>
2017-09-28 15:38:24 +0200

    Completed uppdaterad

--- quiz/views.py
+++ quiz/views.py
@@ -6,6 +6,7 @@ def startpage(request):
 	context = {
 	    	"quizzes": Quiz.objects.all(),
 	}
+
 	return render(request, "start.html", context)
 
 def quiz(request, quiz_number):
@@ -25,18 +26,31 @@ def question(request, quiz_number, question_number):
     		"question": question.question,
 	    	"answer1": question.answer1,
     		"answer2": question.answer2,
-	    	"answer3": question.answer3,
-	    	"quiz": quiz,
+			"answer3": question.answer3,
+			"quiz": quiz,
 		"quiz_number": quiz_number,
 	}
 
 	return render(request, "question.html", context)
 
+
 def completed(request, quiz_number):
+	quiz = Quiz.objects.get(quiz_number=quiz_number)
+	questions = list(quiz.questions.all())
+	saved_answers = request.session.get(str(quiz_number), 
+	{})
+
+	num_correct_answers = 0
+	for question_number, answer in saved_answers.items():
+		correct_answer = questions[int(question_number) - 1].correct
+
+		if correct_answer == answer:
+			num_correct_answers = num_correct_answers + 1
+
+	num_questions = quiz.questions.count()
 	context = {
-	    	"correct": 12,
-	    	"total": 20,
-		"quiz_number": quiz_number,
+			"correct": num_correct_answers,
+			"total": num_questions,
 	}
 	return render(request, "completed.html", context)
 
@@ -45,5 +59,10 @@ def answer(request, quiz_number, question_number):
 	saved_answers = request.session.get(str(quiz_number), {})
 	saved_answers[question_number] = int(answer)
 	request.session[quiz_number] = saved_answers
-	return redirect("question_page", quiz_number, question_number + 1)
 
+	quiz = Quiz.objects.get(quiz_number=quiz_number)
+	num_questions = quiz.questions.count()
+	if num_questions <= question_number:
+		return redirect("completed_page", quiz_number)
+	else:
+		return redirect("question_page", quiz_number, question_number + 1)
